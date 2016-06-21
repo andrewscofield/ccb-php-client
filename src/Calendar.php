e0b7eb1ab3b95c16e06b73ec7931998535eca4e7
Andrew Scofied <apotropaics@gmail.com>
2016-06-20 23:40:05 -0700

    adding calendar functions

--- src/Calendar.php
+++ src/Calendar.php
@@ -4,22 +4,45 @@ use CompassHB\Ccb\Ccb;
 
 class Calendar{
 
-    /**
-     * @param DateTime $date_start
-     * @param DateTime $date_end
-     *
-     * @return ...
-     */
-    public function publicCalendarListing($date_start="2010-01-01", $date_end=NULL){
+  /**
+   * @param DateTime $date_start
+   * @param DateTime $date_end
+   *
+   * @return array of public calendar list items
+   */
+  public function publicCalendarListing($date_start="2010-01-01", $date_end=NULL){
 
-      $args["date_start"] = Ccb::format_date($date_start);;
+    $args["date_start"] = Ccb::format_date($date_start);
 
-      if(!empty($date_end)){
-        $args["date_end"] = Ccb::format_date($date_end);;
-      }
-
-      $calendar_listing = Ccb::$api->get("public_calendar_listing", $args);
-      return $calendar_listing->responseXML()->response->items->item;
+    if(!empty($date_end)){
+      $args["date_end"] = Ccb::format_date($date_end);
     }
+
+    $calendar_listing = Ccb::$api->get("public_calendar_listing", $args);
+    return $calendar_listing->responseXML()->response->items->item;
+  }
+
+
+  /**
+   * @param DateTime $date_start
+   * @param DateTime $date_end
+   *
+   * @return array of public calendar list items
+   */
+  public function individualCalendarListing($individual_id, $date_start="2010-01-01", $date_end=NULL){
+
+    $args["id"] = $individual_id;
+    $args["date_start"] = Ccb::format_date($date_start);
+
+    if(!empty($date_end)){
+      $args["date_end"] = Ccb::format_date($date_end);
+    }
+
+    $calendar_listing = Ccb::$api->get("public_calendar_listing", $args);
+    return $calendar_listing->responseXML()->response->items->item;
+  }
+
+
+
 
 }
