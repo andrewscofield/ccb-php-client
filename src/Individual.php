a500de2ab4f28cb46bbe0233a66958284da63833
Andrew Scofied <apotropaics@gmail.com>
2017-02-02 14:34:56 -0800

    auto formatting php file...

--- src/Individual.php
+++ src/Individual.php
@@ -2,40 +2,41 @@
 
 use CompassHB\Ccb\Ccb;
 
-class Individual{
+class Individual
+{
 
   /**
-   * @param DateTime $date_start
-   * @param DateTime $date_end
+   * @param int $id
    *
    * @return array of public calendar list items
    */
-  public function individualProfileFromId($id){
+    public function individualProfileFromId($id)
+    {
 
-    $individual_profile = Ccb::$api->get("individual_profile_from_id", [
-      "individual_id" => intval($id)
-    ]);
-    return $individual_profile->responseXML()->response->individuals->individual;
-  }
-
-
-  public function updateIndividual($id, $params = array()){
-    $individual_params = array(
-      "individual_id" => intval($id)
-    );
-
-    $updated_individual = Ccb::$api->post("update_individual", $individual_params, $params);
-    return $updated_individual;
-  }
-
-  public function savedSearch($id){
-    $search_params = array(
-      "id" => intval($id)
-    );
-
-    $search_results = Ccb::$api->get("execute_search", $search_params);
-    return $search_results->responseXML()->response->individuals->individual;
-  }
+        $individual_profile = Ccb::$api->get("individual_profile_from_id", [
+        "individual_id" => intval($id)
+        ]);
+        return $individual_profile->responseXML()->response->individuals->individual;
+    }
 
 
+    public function updateIndividual($id, $params = array())
+    {
+        $individual_params = array(
+            "individual_id" => intval($id)
+        );
+
+        $updated_individual = Ccb::$api->post("update_individual", $individual_params, $params);
+        return $updated_individual;
+    }
+
+    public function savedSearch($id)
+    {
+        $search_params = array(
+        "id" => intval($id)
+        );
+
+        $search_results = Ccb::$api->get("execute_search", $search_params);
+        return $search_results->responseXML()->response->individuals->individual;
+    }
 }
