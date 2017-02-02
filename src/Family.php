5e152ef023a396cdc210aaa9de0fd60b22c5b15c
Andrew Scofied <apotropaics@gmail.com>
2017-02-02 11:37:13 -0800

    addding family apis

---
+++ src/Family.php
@@ -0,0 +1,40 @@
+<?php namespace CompassHB\Ccb;
+
+use CompassHB\Ccb\Ccb;
+
+class Family{
+
+  /**
+   * @param int $id
+   *
+   * @return array of public calendar list items
+   */
+  public function familyDetail($id){
+
+    $family_detail = Ccb::$api->get("family_detail", [
+      "family_id" => intval($id)
+    ]);
+    return $family_detail->responseXML()->items->item;
+  }
+
+
+  /**
+   * @param int $id
+   *
+   * @return array of public calendar list items
+   */
+  public function familyList($id = null){
+
+    $family_id = array();
+
+    if(isset($id)){
+      $family_id["family_id"] = intval($id);
+    }
+
+    $family_list = Ccb::$api->get("family_list", $family_id);
+    return $family_list->responseXML()->response->families->family;
+  }
+
+
+
+}
